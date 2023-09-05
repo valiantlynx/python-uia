932f7a5b944ff120e972ef352d18dd4519711ffd
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-09-05 09:15:37 +0200

    enumerate actually removes the data and offers it for you

--- lesson1.py
+++ lesson1.py
@@ -1,11 +1,12 @@
 print('hello world')
 
 #list- indexed array that dont need
-list1 = [1, 2, 3, 4]
-list1 = [0, 2, 3, 4]
+list1 = [1, 2, 3, 4, 'A', "@"]
+list2 = [0, 2, 3, 4]
 
-# for i, data in enumerate(list1):
-#     print(f'at index: {i}, finnes: {list1[data]}')
+for i, data in enumerate(list1):
+    #print(f'at index: {i}, finnes: {data}')
+    pass
 
 def list_of_numbers(some_list: list):
     return sum(some_list)
@@ -14,4 +15,4 @@ def combine_sum(some_list1: list, some_list2: list):
     return list_of_numbers(some_list1) + list_of_numbers(some_list2)
 
 
-print(combine_sum(some_list1=list1, some_list2=list1))
+print(combine_sum(some_list1=list2, some_list2=list2))
