4e7e985da342ac2d51094dd0fcd16bd501c4f087
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-09-05 08:48:56 +0200

    initial commit

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+print('hello world')
+
+#list- indexed array that dont need
+list1 = [1, 2, 3, 4, 'a', '@']
+
+for i, data in enumerate(list1):
+    print(f'at index: {i}, finnes: {list1[data]}')
+
+def list_of_numbers(list):
+    pass
